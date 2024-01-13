1cc42a37d38a302398ab458bbee88f06ca3dc117
Dustin Carlino <dabreegster@gmail.com>
2024-01-13 18:42:18 +0000

    Bring in the controls for the route snapper, and make the set boundary mode look decent

--- web/src/SetBoundaryMode.svelte
+++ web/src/SetBoundaryMode.svelte
@@ -2,6 +2,7 @@
   import type { Feature, Polygon } from "geojson";
   import { RouteTool } from "./common/route_tool";
   import RouteSnapperLayer from "./common/RouteSnapperLayer.svelte";
+  import SnapPolygonControls from "./common/SnapPolygonControls.svelte";
   import SplitComponent from "./SplitComponent.svelte";
   import { app, mode } from "./stores";
 
@@ -12,6 +13,13 @@
     route_tool.editExistingArea(existing);
   } else {
     route_tool.startArea();
+  }
+
+  function onFailure() {
+    $mode = {
+      mode: "network",
+    };
+    route_tool.clearEventListeners();
   }
 
   route_tool.addEventListenerSuccess((feature) => {
@@ -25,23 +33,23 @@
       window.alert(
         "Known georust bug hit, sorry. You may need to just refresh the page now."
       );
-      $mode = {
-        mode: "network",
-      };
-      route_tool.clearEventListeners();
+      onFailure();
     }
   });
-  route_tool.addEventListenerFailure(() => {
-    $mode = {
-      mode: "network",
-    };
-    route_tool.clearEventListeners();
-  });
+  route_tool.addEventListenerFailure(onFailure);
 </script>
 
 <SplitComponent>
   <div slot="sidebar">
-    <p>Draw the boundary...</p>
+    <h1>Draw your neighbourhood boundary</h1>
+    <p>TODO: maybe move the instructions from the previous screen to here...</p>
+
+    <SnapPolygonControls {route_tool} />
+
+    <div>
+      <button on:click={() => route_tool.finish()}>Finish</button>
+      <button on:click={onFailure}>Cancel</button>
+    </div>
   </div>
 
   <div slot="map">
---
+++ web/src/common/SnapPolygonControls.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { RouteTool } from "./route_tool";
+  import { undoLength } from "./stores";
+
+  export let route_tool: RouteTool;
+</script>
+
+<button disabled={$undoLength == 0} on:click={() => route_tool.undo()}>
+  {#if $undoLength == 0}
+    Undo
+  {:else}
+    Undo ({$undoLength})
+  {/if}
+</button>
+
+<ul>
+  <li>
+    <b>Click</b>
+    the map to add points
+  </li>
+  <li>
+    <b>Click and drag</b>
+    any point to move it
+  </li>
+  <li>
+    <b>Click</b>
+    a waypoint to delete it
+  </li>
+  <li>
+    Press <b>Control+Z</b>
+    to undo your last change
+  </li>
+  <li>
+    Press <b>Enter</b>
+    or
+    <b>double click</b>
+    to finish
+  </li>
+  <li>
+    Press <b>Escape</b>
+    to cancel
+  </li>
+</ul>
