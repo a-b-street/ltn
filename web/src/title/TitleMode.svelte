4fc49598b8c0c73b2bd0bdfb9ad64b4c85e0698d
Dustin Carlino <dabreegster@gmail.com>
2024-03-01 16:52:36 +0000

    Reorg loading code more, and get the new project flow to work

--- web/src/title/TitleMode.svelte
+++ web/src/title/TitleMode.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-  import { LngLat } from "maplibre-gl";
-  import { LTN } from "backend";
-  import type { Feature } from "geojson";
-  import { Link, overpassQueryForPolygon } from "../common";
-  import { RouteTool } from "../common/snapper/route_tool";
+  import { Link } from "../common";
   import SplitComponent from "../SplitComponent.svelte";
   import {
     app,
@@ -11,12 +7,10 @@
     map,
     showAbout,
     mode,
-    useLocalVite,
     route_tool,
-    route_pt_a,
-    route_pt_b,
   } from "../stores";
   import About from "./About.svelte";
+  import { loadFromLocalStorage } from "./loader";
 
   export let wasmReady: boolean;
 
@@ -46,70 +40,6 @@
     window.localStorage.setItem(key, await fileInput.files![0].text());
     projectList = getProjectList();
     await loadFromLocalStorage(key);
-  }
-
-  async function loadFromLocalStorage(key: string) {
-    $projectName = key;
-    try {
-      let gj = JSON.parse(window.localStorage.getItem($projectName)!);
-
-      let buffer = await getOsmInput(gj);
-      //msg = "Building map model from OSM input";
-      console.time("load");
-      $app = new LTN(
-        new Uint8Array(buffer),
-        gj.study_area_boundary || undefined,
-      );
-      // TODO rename as project?
-      // TODO or actually, combo this with the constructor.
-      $app.loadSavefile(gj);
-      console.timeEnd("load");
-
-      afterProjectLoaded();
-    } catch (err) {
-      window.alert(`Couldn't open project: ${err}`);
-      $projectName = "";
-    }
-  }
-
-  // Either from a pre-hosted pbf file or from Overpass
-  async function getOsmInput(gj: any): Promise<ArrayBuffer> {
-    if (gj.study_area_name) {
-      let url = $useLocalVite
-        ? `/osm/${gj.study_area_name}.pbf`
-        : `https://assets.od2net.org/severance_pbfs/${gj.study_area_name}.pbf`;
-      //msg = `Downloading ${url}`;
-      let resp = await fetch(url);
-      let bytes = await resp.arrayBuffer();
-      return bytes;
-    } else {
-      let study_area_boundary = gj.features.find(
-        (f: Feature) => f.properties!.kind == "study_area_boundary",
-      )!;
-      let resp = await fetch(overpassQueryForPolygon(study_area_boundary));
-      let bytes = await resp.arrayBuffer();
-      return bytes;
-    }
-  }
-
-  function afterProjectLoaded() {
-    $mode = {
-      mode: "network",
-    };
-    $route_tool = new RouteTool($map!, $app!.toRouteSnapper());
-    $map!.fitBounds(
-      Array.from($app!.getBounds()) as [number, number, number, number],
-      { animate: false },
-    );
-    $route_pt_a = randomPoint();
-    $route_pt_b = randomPoint();
-  }
-
-  function randomPoint(): LngLat {
-    let bounds = $app!.getBounds();
-    let lng = bounds[0] + Math.random() * (bounds[2] - bounds[0]);
-    let lat = bounds[1] + Math.random() * (bounds[3] - bounds[1]);
-    return new LngLat(lng, lat);
   }
 </script>
 
---
+++ web/src/title/loader.ts
@@ -0,0 +1,79 @@
+import { get } from "svelte/store";
+import { LngLat } from "maplibre-gl";
+import { LTN } from "backend";
+import type { Feature } from "geojson";
+import { overpassQueryForPolygon } from "../common";
+import { RouteTool } from "../common/snapper/route_tool";
+import {
+  app,
+  projectName,
+  map,
+  mode,
+  useLocalVite,
+  route_tool,
+  route_pt_a,
+  route_pt_b,
+} from "../stores";
+
+export async function loadFromLocalStorage(key: string) {
+  projectName.set(key);
+  try {
+    let gj = JSON.parse(window.localStorage.getItem(key)!);
+
+    let buffer = await getOsmInput(gj);
+    //msg = "Building map model from OSM input";
+    console.time("load");
+    app.set(
+      new LTN(new Uint8Array(buffer), gj.study_area_boundary || undefined),
+    );
+    // TODO rename as project?
+    // TODO or actually, combo this with the constructor.
+    get(app)!.loadSavefile(gj);
+    console.timeEnd("load");
+
+    afterProjectLoaded();
+  } catch (err) {
+    window.alert(`Couldn't open project: ${err}`);
+    projectName.set("");
+  }
+}
+
+// Either from a pre-hosted pbf file or from Overpass
+async function getOsmInput(gj: any): Promise<ArrayBuffer> {
+  if (gj.study_area_name) {
+    let url = get(useLocalVite)
+      ? `/osm/${gj.study_area_name}.pbf`
+      : `https://assets.od2net.org/severance_pbfs/${gj.study_area_name}.pbf`;
+    //msg = `Downloading ${url}`;
+    let resp = await fetch(url);
+    let bytes = await resp.arrayBuffer();
+    return bytes;
+  } else {
+    let study_area_boundary = gj.features.find(
+      (f: Feature) => f.properties!.kind == "study_area_boundary",
+    )!;
+    let resp = await fetch(overpassQueryForPolygon(study_area_boundary));
+    let bytes = await resp.arrayBuffer();
+    return bytes;
+  }
+}
+
+export function afterProjectLoaded() {
+  mode.set({
+    mode: "network",
+  });
+  route_tool.set(new RouteTool(get(map)!, get(app)!.toRouteSnapper()));
+  get(map)!.fitBounds(
+    Array.from(get(app)!.getBounds()) as [number, number, number, number],
+    { animate: false },
+  );
+  route_pt_a.set(randomPoint());
+  route_pt_b.set(randomPoint());
+}
+
+function randomPoint(): LngLat {
+  let bounds = get(app)!.getBounds();
+  let lng = bounds[0] + Math.random() * (bounds[2] - bounds[0]);
+  let lat = bounds[1] + Math.random() * (bounds[3] - bounds[1]);
+  return new LngLat(lng, lat);
+}
