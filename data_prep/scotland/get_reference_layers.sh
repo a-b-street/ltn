0dfe2e8107c4fb3c3b43520197efb49f02458eeb
Dustin Carlino <dabreegster@gmail.com>
2025-02-07 16:14:45 +0000

    Population data will need more work before displaying, so remove for now

--- data_prep/scotland/get_reference_layers.sh
+++ data_prep/scotland/get_reference_layers.sh
@@ -46,25 +46,11 @@ function cbd {
   wget https://nptscot.blob.core.windows.net/pmtiles/cbd_layer_2024-12-01.pmtiles -O $OUT/cbd.pmtiles
 }
 
-function population {
-  # From https://www.data.gov.uk/dataset/1102bf85-ed49-440a-b211-da87e8d752eb/scottish-index-of-multiple-deprivation-simd-2020
-  wget https://maps.gov.scot/ATOM/shapefiles/SG_SIMD_2020.zip
-  unzip SG_SIMD_2020.zip
-  ogr2ogr tmp/population.geojson \
-          -t_srs EPSG:4326 \
-          SG_SIMD_2020.shp \
-          -nlt PROMOTE_TO_MULTI \
-          -sql 'SELECT DataZone, Rankv2 as rank, Percentv2 as percentile, SAPE2017 as population, OGR_GEOM_AREA as area FROM SG_SIMD_2020'
-  rm -f SG_SIMD_2020* SIMD2020v2*xlsx
-  tippecanoe tmp/population.geojson --generate-ids -l population -o $out/population.pmtiles
-}
-
 # Uncomment each as needed
 #route_network
 #schools
 #gp_and_hospitals ~/Downloads/GP_Practices_-_Scotland.json ~/Downloads/NHS_Hospitals_-_Scotland.json
 #cbd
-#population
 
 echo "For maintainer only:"
 echo "  mv $OUT/* ~/cloudflare_sync/cnt_layers/"
