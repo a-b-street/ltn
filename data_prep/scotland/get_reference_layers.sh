0615c0f0dac8674ee17fde6dfd31820874cee5b8
Dustin Carlino <dabreegster@gmail.com>
2025-02-09 17:06:26 +0000

    Add a railway station layer.

--- data_prep/scotland/get_reference_layers.sh
+++ data_prep/scotland/get_reference_layers.sh
@@ -35,15 +35,37 @@ function gp_and_hospitals {
           -sql 'SELECT sitename AS name FROM "NHS_Hospitals_-_Scotland"'
 
   # The bboxes or something else included are breaking parsing, so clean these up
-  jq '{ type: "FeatureCollection", features: [.features[] | { type: "Feature", geometry: .geometry, properties: { name: .properties.name } }] }' tmp/gp_practices.geojson > tmp.gj
-  mv -f tmp.gj $OUT/gp_practices.geojson
+  jq '{ type: "FeatureCollection", features: [.features[] | { type: "Feature", geometry: .geometry, properties: { name: .properties.name } }] }' tmp/gp_practices.geojson > $OUT/gp_practices.geojson
 
-  jq '{ type: "FeatureCollection", features: [.features[] | { type: "Feature", geometry: .geometry, properties: { name: .properties.name } }] }' tmp/hospitals.geojson > tmp.gj
-  mv -f tmp.gj $OUT/hospitals.geojson
+  jq '{ type: "FeatureCollection", features: [.features[] | { type: "Feature", geometry: .geometry, properties: { name: .properties.name } }] }' tmp/hospitals.geojson > $OUT/hospitals.geojson
 }
 
 function cbd {
   wget https://nptscot.blob.core.windows.net/pmtiles/cbd_layer_2024-12-01.pmtiles -O $OUT/cbd.pmtiles
+}
+
+function railway_stations {
+  get_scotland_osm
+  osmium tags-filter tmp/scotland-latest.osm.pbf n/railway=station -o tmp/railways.osm.pbf
+  osmium export tmp/railways.osm.pbf -o tmp/railways.geojson
+  # Strip most tag
+  jq '{ type: "FeatureCollection", features: [.features[] | { type: "Feature", geometry: .geometry, properties: { name: .properties.name } }] }' tmp/railways.geojson > $OUT/railways.geojson
+}
+
+function get_scotland_osm {
+  # Download Scotland OSM data
+  if [ ! -f tmp/scotland-latest.osm.pbf ]; then
+    download_to_subdir tmp https://download.geofabrik.de/europe/united-kingdom/scotland-latest.osm.pbf
+  fi
+}
+
+download_to_subdir() {
+  local subdir=$1
+  local url=$2
+
+  mkdir -p "$subdir"
+  (wget -P "$subdir" --timestamping "$url" && echo "✅ $url") \
+    || echo "❌ Download failed: $url"
 }
 
 # Uncomment each as needed
@@ -51,6 +73,7 @@ function cbd {
 #schools
 #gp_and_hospitals ~/Downloads/GP_Practices_-_Scotland.json ~/Downloads/NHS_Hospitals_-_Scotland.json
 #cbd
+#railway_stations
 
 echo "For maintainer only:"
 echo "  mv $OUT/* ~/cloudflare_sync/cnt_layers/"
