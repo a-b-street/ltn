2374f21bfc06f05d8eab4089703609fbc4ef471c
Dustin Carlino <dabreegster@gmail.com>
2025-02-23 12:08:03 +0000

    Count the number of stats19 collisions in a boundary

--- data_prep/scotland/src/bin/generate_prioritization.rs
+++ data_prep/scotland/src/bin/generate_prioritization.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use backend::boundary_stats::{ContextData, PopulationZone};
 use data_prep::StudyArea;
-use geo::{MultiPolygon, PreparedGeometry, Relate};
+use geo::{MultiPolygon, Point, PreparedGeometry, Relate};
 use serde::Deserialize;
 use std::time::Instant;
 
@@ -13,12 +13,14 @@ fn main() -> Result<()> {
     println!("Time since start {:?}", start.elapsed());
     let population_zone_inputs = PopulationZoneInput::read_all_prepared_from_file()?;
     println!("Time since start {:?}", start.elapsed());
+    let stats19_collisions = Stats19Input::read_all_from_file()?;
 
     for study_area in &study_areas {
         let study_area_start = Instant::now();
         println!("Starting {}", study_area.1.name);
         let mut context_data = ContextData {
             population_zones: Vec::new(),
+            stats19_collisions: Vec::new(),
         };
 
         for population_zone_input in &population_zone_inputs {
@@ -32,6 +34,12 @@ fn main() -> Result<()> {
                     imd_percentile: population_zone_input.1.imd_percentile,
                     population: population_zone_input.1.population,
                 });
+            }
+        }
+
+        for pt in &stats19_collisions {
+            if study_area.0.relate(&pt.geometry).is_contains() {
+                context_data.stats19_collisions.push(pt.geometry);
             }
         }
 
@@ -54,9 +62,9 @@ fn main() -> Result<()> {
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct PopulationZoneInput {
+struct PopulationZoneInput {
     #[serde(deserialize_with = "geojson::de::deserialize_geometry")]
-    pub geometry: MultiPolygon,
+    geometry: MultiPolygon,
 
     // "id": "S01006506",
     // (unused)
@@ -65,10 +73,10 @@ pub struct PopulationZoneInput {
     // (unused)
 
     // "imd_percentile": 68,
-    pub imd_percentile: u8,
+    imd_percentile: u8,
 
     // "population": 894,
-    pub population: u32,
+    population: u32,
     // "area": 4388802.1221970674
     // (unused - though maybe we would find it helpful for pre-computing density or to save the cost of calculating area live)
 }
@@ -76,7 +84,6 @@ pub struct PopulationZoneInput {
 impl PopulationZoneInput {
     fn read_all_from_file() -> Result<Vec<Self>> {
         let population_zones = geojson::de::deserialize_feature_collection_str_to_vec(
-            // REVIEW: I think it's OK to just explode with useful error messages for the data_prep stuff, right?
             &std::fs::read_to_string("tmp/population.geojson")
                 .expect("missing population.geojson - see get_reference_layers.sh"),
         )?;
@@ -94,5 +101,21 @@ impl PopulationZoneInput {
                 )
             });
         Ok(iter.collect())
+    }
+}
+
+// Ignore all properties
+#[derive(Deserialize)]
+struct Stats19Input {
+    #[serde(deserialize_with = "geojson::de::deserialize_geometry")]
+    geometry: Point,
+}
+
+impl Stats19Input {
+    fn read_all_from_file() -> Result<Vec<Self>> {
+        Ok(geojson::de::deserialize_feature_collection_str_to_vec(
+            &std::fs::read_to_string("atip-data-prep/stats19/tmp/stats19_clipped.geojson")
+                .expect("missing stats19_clipped.geojson - see get_reference_layers.sh"),
+        )?)
     }
 }
