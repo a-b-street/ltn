8ad4caf069864fc503989a6d17782465d5708e5b
Michael Kirk <michael.code@endoftheworl.de>
2025-03-20 11:09:44 -0700

    Clearer steps for "add neighbourhood".
    We now keep the steps visible at all times, and rely on "inactive" state
    to communicate which can be acted upon currently.
    
    We also highlight the "active" step to nudge the user as to what they
    need to do next.
    
    Added some hints for invalid geometry drawings.
    
    You can now "start over" your drawing even when the shape isn't
    valid - previously you could only "start over" to clear your boundary,
    but if you don't have a valid shape you can't get a boundary.

--- web/src/common/NeighbourhoodBoundarySummary.svelte
+++ web/src/common/NeighbourhoodBoundarySummary.svelte
@@ -6,63 +6,60 @@
   export let neighbourhoodBoundary: GeneratedBoundaryFeature;
 </script>
 
-<div class="container">
-  <table>
+<table>
+  <tr>
+    <th>Area</th>
+    <td>
+      {neighbourhoodBoundary.properties.area_km2.toFixed(1)} km²
+    </td>
+  </tr>
+  {#if $appFocus == "cnt"}
     <tr>
-      <th>Area</th>
+      <th>Population density</th>
       <td>
-        {neighbourhoodBoundary.properties.area_km2.toFixed(1)} km²
+        {Math.round(
+          neighbourhoodBoundary.properties.population /
+            neighbourhoodBoundary.properties.area_km2,
+        ).toLocaleString()} people / km²
       </td>
     </tr>
-    {#if $appFocus == "cnt"}
-      <tr>
-        <th>Population density</th>
-        <td>
-          {Math.round(
-            neighbourhoodBoundary.properties.population /
-              neighbourhoodBoundary.properties.area_km2,
-          ).toLocaleString()} people / km²
-        </td>
-      </tr>
-      <tr>
-        <th>SIMD</th>
-        <td>{neighbourhoodBoundary.properties.simd.toFixed(1)}%</td>
-      </tr>
-      <tr>
-        <th>Collision density</th>
-        <td>
-          {(
-            neighbourhoodBoundary.properties.number_stats19_collisions /
-            neighbourhoodBoundary.properties.area_km2
-          ).toFixed(1)} / km²
-        </td>
-      </tr>
-      <tr>
-        <th>Car ownership</th>
-        <td>
-          {prettyPrintPercent(
-            neighbourhoodBoundary.properties.households_with_cars_or_vans,
-            neighbourhoodBoundary.properties.total_households,
-          )} of households
-        </td>
-      </tr>
-      <tr>
-        <th>POI density</th>
-        <td>
-          {(
-            neighbourhoodBoundary.properties.number_pois /
-            neighbourhoodBoundary.properties.area_km2
-          ).toFixed(1)} / km²
-        </td>
-      </tr>
-    {/if}
-  </table>
-</div>
+    <tr>
+      <th>SIMD</th>
+      <td>{neighbourhoodBoundary.properties.simd.toFixed(1)}%</td>
+    </tr>
+    <tr>
+      <th>Collision density</th>
+      <td>
+        {(
+          neighbourhoodBoundary.properties.number_stats19_collisions /
+          neighbourhoodBoundary.properties.area_km2
+        ).toFixed(1)} / km²
+      </td>
+    </tr>
+    <tr>
+      <th>Car ownership</th>
+      <td>
+        {prettyPrintPercent(
+          neighbourhoodBoundary.properties.households_with_cars_or_vans,
+          neighbourhoodBoundary.properties.total_households,
+        )} of households
+      </td>
+    </tr>
+    <tr>
+      <th>POI density</th>
+      <td>
+        {(
+          neighbourhoodBoundary.properties.number_pois /
+          neighbourhoodBoundary.properties.area_km2
+        ).toFixed(1)} / km²
+      </td>
+    </tr>
+  {/if}
+</table>
 
 <style>
-  .container {
-    border: dashed black 2px;
-    border-radius: 8px;
-    margin-bottom: 16px;
+  table {
+    margin: 0;
+    padding: 0;
   }
 </style>
