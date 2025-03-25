40784a054bf43be186a1b22d51e67e0ac9f1446f
dabreegster <dabreegster@users.noreply.github.com>
2025-03-25 07:20:43 +0000

    deploy: b0c0bfdc5ffa7b10a2f6ef8972ccba9686737657

--- assets/cnt-17ac0a0c.js
+++
@@ -1,1 +0,0 @@
-import{A as p}from"./App-02378955.js";new p({target:document.getElementById("app"),props:{appFocus:"cnt"}});
---
+++ assets/main-d7a82a4b.js
@@ -0,0 +1,1 @@
+import{A as e}from"./App-7422fc72.js";new e({target:document.getElementById("app")});
