5842be49107ecc552d8a39571550fd95a4f31e95
dabreegster <dabreegster@users.noreply.github.com>
2025-04-09 14:57:00 +0000

    deploy: 10936af481795096e67f194be4df73705acbb060

--- assets/main-20eb99c6.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./App-9dd7d06a.js";new e({target:document.getElementById("app")});
---
+++ assets/main-8f772efe.js
@@ -0,0 +1,1 @@
+import{A as e}from"./App-dcdc5a93.js";new e({target:document.getElementById("app")});
