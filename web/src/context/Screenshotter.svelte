da9737a66b7c97cc3bddbc3e3dcacd1075257315
Dustin Carlino <dabreegster@gmail.com>
2025-10-12 13:29:52 +0100

    Do a few in a grid

--- web/src/context/Screenshotter.svelte
+++ web/src/context/Screenshotter.svelte
@@ -1,17 +1,48 @@
 <script lang="ts">
-  import { map } from "../stores";
+  import type { Map } from "maplibre-gl";
+  import { map as mapStore } from "../stores";
 
-  function start() {
-    $map.redraw();
+  async function start() {
+    let map = $mapStore!;
 
-    $map.once("idle", () => {
-      $map.getCanvas().toBlob((blob) => {
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(blob);
-        a.download = "screenshot.png";
-        a.click();
+    let canvas = map.getCanvas();
+    let width = canvas.width;
+    let height = canvas.height;
+
+    // Just try a manual 2x2
+    await takeScreenshot(map, "row1col1.png");
+    panByPixels(map, width, 0);
+    await takeScreenshot(map, "row1col2.png");
+    panByPixels(map, -width, height);
+    await takeScreenshot(map, "row2col1.png");
+    panByPixels(map, width, 0);
+    await takeScreenshot(map, "row2col2.png");
+  }
+
+  function takeScreenshot(map: Map, filename: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      map.redraw();
+      map.once("idle", () => {
+        map.getCanvas().toBlob((blob) => {
+          const a = document.createElement("a");
+          a.href = URL.createObjectURL(blob);
+          a.download = filename;
+          a.click();
+
+          resolve();
+        });
       });
     });
+  }
+
+  function panByPixels(map: Map, dx: number, dy: number) {
+    let centerScreen = map.project(map.getCenter());
+    map.setCenter(
+      map.unproject({
+        x: centerScreen.x + dx,
+        y: centerScreen.y + dy,
+      }),
+    );
   }
 </script>
 
